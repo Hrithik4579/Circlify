9df3953e883263714e2b7dec5b4416da59770a14
Hrithik4579 <hbhagat2003@gmail.com>
2024-07-16 21:06:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Content from "./pages/Content";
+import Avatar from "./pages/Avatar";
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Router>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Content />} />
+        <Route path="/avatar" element={<Avatar/>}/>
+      </Routes>
+    </Router>
     </div>
-  );
+  )
 }
-
-export default App;
---
+++ src/pages/Avatar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useState, useEffect } from "react";
+import axios from "axios";
+import styled from "styled-components";
+import { useNavigate} from "react-router-dom";
+import Logo from "../assets/logo.jpg";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+export default function Avatar() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
