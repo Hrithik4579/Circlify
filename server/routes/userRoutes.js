418233b1bd8ff72df30fd1a8a84137e62bc9f1f8
Hrithik4579 <hbhagat2003@gmail.com>
2024-07-19 00:53:25 +0530

    changes

--- server/routes/userRoutes.js
+++
@@ -1,7 +0,0 @@
-const {register,login,avatar}= require("../controllers/userController")
-const router = require("express").Router();
-
-router.post("/register", register);
-router.post("/login",login);
-router.post("/avatar/:id",avatar);
-module.exports=router; 
